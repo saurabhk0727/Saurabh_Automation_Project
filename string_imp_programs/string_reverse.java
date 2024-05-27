89d4a01a19eb1a0b7f9072c745f621b3a7d24783
saurabhk0727 <saurabhkarale0727@gmail.com>
2024-05-27 14:47:52 +0530

    string program

---
+++ string_imp_programs/string_reverse.java
@@ -0,0 +1,12 @@
+package Saurabh_Automation_Project.string_imp_programs;
+
+public class string_reverse {
+    public static void main(String[] args){
+        String s="StringReverse"; String o="";
+        char [] c=s.toCharArray();
+        for (int i=c.length-1; i>0; i--){
+            o=o+c[i];
+        }
+        System.out.println(o);
+    }
+}
